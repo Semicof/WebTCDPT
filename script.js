35b00948415d3d53a23e1caee959701a7a5aa495
Tung <TungLV.B20PT185@stu.ptit.edu.vn>
2022-11-23 22:01:45 +0700

    new

---
+++ script.js
@@ -0,0 +1,61 @@
+function Validator(options) {
+    function validate(inputElement, rule) {
+        var errorMessage = rule.test(inputElement.value)
+        var errorElement = inputElement.parentElement.querySelector(options.errorSelector)
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('invalid');
+        }
+        else {
+            errorElement.innerText = '';
+            inputElement.parentElement.classList.remove('invalid')
+        }
+    }
+    var formElement = document.querySelector(options.form);
+    if (formElement) {
+        options.rules.forEach(function (rule) {
+            var inputElement = formElement.querySelector(rule.selector)
+            if (inputElement) {
+                inputElement.onblur = function () {
+                    validate(inputElement, rule);
+                }
+
+                inputElement.oninput = function () {
+                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector)
+                    errorElement.innerText = '';
+                    inputElement.parentElement.classList.remove('invalid');
+                }
+            }
+        })
+    }
+}
+
+Validator.isRequired = function (selector) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.trim() ? undefined : 'Vui lòng nhập trường này'
+        }
+    }
+}
+
+Validator.isEmail = function (selector) {
+    return {
+        selector: selector,
+        test: function (value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : 'Trường này phải là email'
+        }
+    }
+}
+
+Validator.minLength = function (selector, min) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.length >= min ? undefined : `Vui lòng nhập tối thiểu ${min} ký tự`
+        }
+    }
+}
+
